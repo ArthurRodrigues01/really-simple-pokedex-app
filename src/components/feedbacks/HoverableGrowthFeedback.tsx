a8431a70b5b4ab8ee79276f4ea52c39dae9ca64f
Arthur Rodrigues <art.costa.2005@gmail.com>
2024-04-28 23:51:26 -0300

    fixed: resolve console errors

--- src/components/feedbacks/HoverableGrowthFeedback.tsx
+++ src/components/feedbacks/HoverableGrowthFeedback.tsx
@@ -1,19 +1,8 @@
 import { ReactNode } from "react"
 import styled from "styled-components"
 
-const GrowOnHover = styled.span<{ 
-  borderRadius: number, 
-  borderTLRadius: number, 
-  borderTRRadius: number, 
-  borderBLRadius: number, 
-  borderBRRadius: number 
-}>`
+const GrowOnHover = styled.span`
   transition: .2s;
-  border-top-right-radius: ${props => props.borderTRRadius}px;
-  border-top-left-radius: ${props => props.borderTLRadius}px;
-  border-bottom-right-radius: ${props => props.borderBRRadius}px;
-  border-bottom-left-radius: ${props => props.borderBLRadius}px;
-  ${props => props.borderRadius ? `border-radius: ${props.borderRadius}px;` : ''}
   &: hover {
     outline: 3px solid #fff;
     z-index: 999;
@@ -38,11 +27,13 @@ function HoverableGrowthFeedback({
 }) {
   return (
     <GrowOnHover 
-      borderRadius={borderRadius ? borderRadius : 0} 
-      borderBLRadius={borderBottomLeftRadius ? borderBottomLeftRadius : 0}
-      borderTLRadius={borderTopLeftRadius ? borderTopLeftRadius : 0}
-      borderBRRadius={borderBottomRightRadius ? borderBottomRightRadius : 0}
-      borderTRRadius={borderTopRightRadius ? borderTopRightRadius : 0}
+      style={{
+        borderRadius: borderRadius ?? 0,
+        borderBottomRightRadius: borderBottomRightRadius ?? 0,
+        borderBottomLeftRadius: borderBottomLeftRadius ?? 0,
+        borderTopRightRadius: borderTopRightRadius ?? 0,
+        borderTopLeftRadius: borderTopLeftRadius ?? 0
+      }}
     >
       { children }
     </GrowOnHover>
